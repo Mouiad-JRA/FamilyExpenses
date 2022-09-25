f3db86e558a9b2327c6b46ce103ce73530eaa7fb
mouiad <mouiad.alilo@gmail.com>
2022-09-25 22:01:02 +0300

    feat: add expense app

---
+++ expense/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+
+from expense.views import index, add_expense
+
+urlpatterns = [
+    path('', index, name='expenses'),
+    path('add-expense', add_expense, name='add_expense'),
+]
+
