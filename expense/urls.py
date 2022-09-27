9d2320e71d914d56a49fe413c5c4ccfa1dac3d84
mouiad <mouiad.alilo@gmail.com>
2022-09-27 19:36:11 +0300

    feat: add login , register , set and reset password templates

--- expense/urls.py
+++ expense/urls.py
@@ -5,6 +5,6 @@ from expense.views import index, add_expense
 
 urlpatterns = [
     path('', index, name='expenses'),
-    path('add-expense', add_expense, name='add_expense'),
+    path('add-expense/', add_expense, name='add_expense'),
 ]
 
