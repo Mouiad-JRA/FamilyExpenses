36023c5125e3f3a849d3398ca43a333766ce4f9e
mouiad <mouiad.alilo@gmail.com>
2022-09-30 18:00:16 +0300

    feat: add preferences logic

--- userperferences/views.py
+++ userperferences/views.py
@@ -1,6 +1,33 @@
+import os
+import json
+
+from django.conf import settings
+from django.contrib import messages
 from django.shortcuts import render
 
-# Create your views here.
+from userperferences.models import UserPerference
+
 
 def index(request):
-    return render(request, 'perferences/index.html')
+    currencies = []
+    path = os.path.join(settings.BASE_DIR, 'currencies.json')
+    with open(path) as file:
+        data = json.load(file)
+        for key, value in data.items():
+            currencies.append({"name": key, "value": value})
+    user_perferences = UserPerference.objects.get(user=request.user) if \
+        UserPerference.objects.filter(user=request.user).exists() else None
+    if request.method == 'GET':
+
+        return render(request, 'perferences/index.html',
+                      {'currencies': currencies, 'user_perferences': user_perferences})
+    else:
+        currency = request.POST['currency']
+        if user_perferences:
+            user_perferences.currency = currency
+            user_perferences.save()
+        else:
+            UserPerference.objects.create(user=request.user, currency=currency)
+        messages.success(request, f'Your currency has been changed to {currency}')
+        return render(request, 'perferences/index.html',
+                      {'currencies': currencies, 'user_perferences': user_perferences})
