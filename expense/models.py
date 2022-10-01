671fd07fe15b9a1a5f8b9de6a8e3003587cf979a
mouiad <mouiad.alilo@gmail.com>
2022-10-01 21:08:33 +0300

    feat: add Material, OutlayType and Outlay models

--- expense/models.py
+++ expense/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+from accounts.models import User
+
+
+class Material(models.Model):
+    name = models.CharField(max_length=255)
+    is_service = models.BooleanField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f"This Material name is {self.name}"
+
+
+class OutlayType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Outlay(models.Model):
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    outlay_type = models.ForeignKey(OutlayType, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=7, decimal_places=3)
+    date = models.DateField(default=now())
+    description = models.TextField()
+
+    def __str__(self):
+        return f"This Expense for{self.description} which belong to {self.owner.username} with amount {self.price}"
+
+    class Meta:
+        ordering: ['-data']
