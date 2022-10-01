63c9c5c0180598513969f9943d185ff54e051e74
mouiad <mouiad.alilo@gmail.com>
2022-10-01 21:23:45 +0300

    feat: fill the template with data

--- expense/views.py
+++ expense/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
+from expense.models import OutlayType, Material
+
 
 def index(request):
+    outlay_type = OutlayType.objects.all()
+    materials = Material.objects.all()
     return render(request, 'expenses/index.html')
 
 
 def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
+    outlay_types = OutlayType.objects.all()
+    materials = Material.objects.all()
+    ctx={
+        'outlay_types': outlay_types,
+        'materials' :  materials
+    }
+    return render(request, 'expenses/add_expense.html', ctx)
