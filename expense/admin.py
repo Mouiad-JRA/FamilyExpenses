78e865d8dbb3a6ac2d6c7e522ce4663f866a49ba
mouiad <mouiad.alilo@gmail.com>
2022-10-01 21:09:17 +0300

    feat: register models in Django admin.

--- expense/admin.py
+++ expense/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from expense.models import Outlay, OutlayType, Material
+
+admin.site.register(Material)
+admin.site.register(OutlayType)
+admin.site.register(Outlay)
+
