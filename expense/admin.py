9fca2e66e6deb901615348d421c163fa31c6cf39
mouiad <mouiad.alilo@gmail.com>
2022-10-09 01:38:56 +0300

    feat: custom admin

--- expense/admin.py
+++ expense/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
 from expense.models import Outlay, OutlayType, Material
+
+
+class OutlayAdmin(admin.ModelAdmin):
+    list_display = ('price', 'material', 'outlay_type', 'owner', 'date', 'date')
+    search_fields = ('price', 'material', 'outlay_type', 'owner', 'date', 'date')
+    list_per_page = 5
+
 
 admin.site.register(Material)
 admin.site.register(OutlayType)
-admin.site.register(Outlay)
-
+admin.site.register(Outlay, OutlayAdmin)
