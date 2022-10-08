efcb6c2c98c7964fc3d90b932db476022e26bc2f
mouiad <mouiad.alilo@gmail.com>
2022-10-08 14:49:42 +0300

    feat: add search

--- FamilyExpenses/static/js/searchOutlays.js
+++ FamilyExpenses/static/js/searchOutlays.js
@@ -1,12 +1,16 @@
 const searchField = document.querySelector('#id_search')
 const tableOutput = document.querySelector('.table-output')
 const appTable = document.querySelector('.app-table')
+const pagination = document.querySelector('.pagination-container')
+const tableBody = document.querySelector('.table-boday')
 
 tableOutput.style.display = "none";
 
 searchField.addEventListener('keyup',(e)=>{
     const searchValue = e.target.value;
     if(searchValue.trim().length>0){
+        pagination.style.display = "none";
+        tableBody.innerHTML="";
      fetch("search-expenses", {
          body:JSON.stringify( {searchText: searchValue }),
          method: "POST",
@@ -15,10 +19,47 @@ searchField.addEventListener('keyup',(e)=>{
         ).then(data => {
            console.log('data',data)
          appTable.style.display = "none"
-         tableOutput.style.display = 'block'
+         tableOutput.style.display = "block";
          if(data.length==0){
-                tableOutput.innerHTMl = "No results Found"
+         tableOutput.innerHTML = "No results Found";
+         }else{
+             data.forEach(outlay=>{
+                 console.log(outlay)
+             tableBody.innerHTML+=`
+          <tr>
+            <td>
+                ${outlay.price}
+            </td>
+            <td>
+                ${outlay.outlay_type_id}
+            </td>
+            <td>
+                ${outlay.material_id}
+            </td>
+            <td>
+               ${outlay.description}
+            </td>
+            <td>
+                ${outlay.date}
+            </td>
+                     <th><a href="{% url "expenses-dash:delete" outlay.id %}" class="btn btn-danger btn-sm">
+             {% trans "Delete" %}</a> </th>
+         <th><a href="{% url "expenses-dash:edit" outlay.id %}" class="btn btn-secondary btn-sm">
+             {% trans "Edit" %}</a> </th>
+
+        </tr>
+                `
+             })
+
          }
         });
+    }
+    else{
+        tableOutput.style.display ="none";
+        appTable.style.display = "block";
+        pagination.style.display = "block";
+
+
+
     }
 })
--- expense/urls.py
+++ expense/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 
-from expense.views import index, delete_expense, ExpenseCreateView, ExpenseEditView, search_expenses
+from expense.views import delete_expense, ExpenseCreateView, ExpenseEditView, search_expenses, \
+    MaterialCreateView, ExpenseListView, MaterialListView, MaterialEditView, delete_material
 from django.views.decorators.csrf import csrf_exempt
 app_name = "expenses-dash"
 urlpatterns = [
-    path('', index, name='expenses'),
+    path('', ExpenseListView.as_view(), name='expenses'),
     path('search-expenses', csrf_exempt(search_expenses), name='search_expenses'),
     path('add-expense/', ExpenseCreateView.as_view(), name='add_expense'),
-    path('edit-expense/<int:pk>', ExpenseEditView.as_view(), name='edit'),
-    path('delete-expense/<int:pk>', delete_expense, name='delete'),
+    path('edit-expense/<int:pk>', ExpenseEditView.as_view(), name='edit_expense'),
+    path('delete-expense/<int:pk>', delete_expense, name='delete_expense'),
+    path('materials/', MaterialListView.as_view(), name='materials'),
+    path('add-material/', MaterialCreateView.as_view(), name='add_material'),
+    path('edit-material/<int:pk>', MaterialEditView.as_view(), name='edit_material'),
+    path('delete-material/<int:pk>', delete_material, name='delete_material'),
 ]
 
